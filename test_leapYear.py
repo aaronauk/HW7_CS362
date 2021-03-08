9636a1d0170d32de3f2fbe55317a2522f20dda1c
Aaron <aaron967@comcast.net>
2021-03-07 21:17:17 -0800

    Wrote second test, that tests leapYear function with an input of 2008 that expects True

--- test_leapYear.py
+++ test_leapYear.py
@@ -4,6 +4,8 @@ import leapYear
 class LeapYearTest(unittest.TestCase):
     def test_leapYear1(self):
         self.assertEqual(leapYear.leapYear(2003), False)
-
+    def test_leapYear2(self):
+        self.assertEqual(leapYear.leapYear(2008), True)
+        
 if __name__ == "__main__":
     unittest.main()  
