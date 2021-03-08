070125b82c8a2636837c599aca5aef9d23f5db1f
Aaron <aaron967@comcast.net>
2021-03-07 21:14:50 -0800

    Wrote fifth test, that tests FizzBuzz function with an input of 105 and expects None to be returned

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -11,6 +11,8 @@ class FizzBuzzTest(unittest.TestCase):
         self.assertEqual(fizzBuzz.fizzBuzz(10), 'Buzz')
     def test_fizzBuzz4(self):
         self.assertEqual(fizzBuzz.fizzBuzz(2), 2)
+    def test_fizzBuzz5(self):
+        self.assertEqual(fizzBuzz.fizzBuzz(105), None)
 
 if __name__ == "__main__":
     unittest.main()  
