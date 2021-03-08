c40a046b7136094ec037bfbf6a0ed0cb56aae503
Aaron <aaron967@comcast.net>
2021-03-07 21:19:26 -0800

    Created fourth test which test leapYear function with input of 2100 that is expected to return False

--- test_leapYear.py
+++ test_leapYear.py
@@ -8,6 +8,8 @@ class LeapYearTest(unittest.TestCase):
         self.assertEqual(leapYear.leapYear(2008), True)
     def test_leapYear3(self):
         self.assertEqual(leapYear.leapYear(2400), True)
-            
+    def test_leapYear4(self):
+        self.assertEqual(leapYear.leapYear(2100), False)
+
 if __name__ == "__main__":
     unittest.main()  
