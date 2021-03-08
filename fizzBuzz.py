ddd9886aa536ffa4acea315a9c4843b63154dcaa
Aaron <aaron967@comcast.net>
2021-03-07 21:15:35 -0800

    Implemented FizzBuzz function to return None if input is not between 1 and 100.

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,11 +1,14 @@
 import sys
 
 def fizzBuzz(n):
-    if (n % 3 == 0 and n % 5 == 0):
-        return 'FizzBuzz'  
-    elif (n % 3 == 0):
-        return 'Fizz'
-    elif (n % 5 == 0):
-        return 'Buzz'
+    if (1 <= n <= 100):
+        if (n % 3 == 0 and n % 5 == 0):
+            return 'FizzBuzz'  
+        elif (n % 3 == 0):
+            return 'Fizz'
+        elif (n % 5 == 0):
+            return 'Buzz'
+        else:
+            return n
     else:
-        return n
+        return None
