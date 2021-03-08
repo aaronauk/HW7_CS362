2c7255cbaceef7dd75a9320963c33c488e2d6256
Aaron <aaron967@comcast.net>
2021-03-07 21:11:41 -0800

    Wrote second test, which tests FizzBuzz function with an input of 6 and expects 'Fizz' to be printed

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -5,6 +5,8 @@ import fizzBuzz
 class FizzBuzzTest(unittest.TestCase):
     def test_fizzBuzz1(self):
         self.assertEqual(fizzBuzz.fizzBuzz(15), 'FizzBuzz')
-
+    def test_fizzBuzz2(self):
+        self.assertEqual(fizzBuzz.fizzBuzz(6), 'Fizz')
+        
 if __name__ == "__main__":
     unittest.main()  
